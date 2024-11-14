fbe9ab9797c0d07946859846e4ef62b71ecacca4
gerry1002 <pillogeremia@yahoo.com>
2024-11-14 16:27:32 +0100

    Test Python

--- Backend/server.py
+++ Backend/server.py
@@ -1,4 +1,11 @@
-print("Test")
-x = 10
-print(x)
-print('cacacaca')
+import mysql.connector
+
+config = {
+  'user': 'root',
+  'password': 'root',
+  'host': 'localhost:3306',
+  'database': 'inventory',
+  'raise_on_warnings': True,
+}
+
+link = mysql.connector.connect(**config)
